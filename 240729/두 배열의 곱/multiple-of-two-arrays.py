1b4534dad2f587c2af884c1f75308d0170666494
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-07-29 19:38:51 +0900

    Title: 두 배열의 곱, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240729/두 배열의 곱/multiple-of-two-arrays.py
+++ 240729/두 배열의 곱/multiple-of-two-arrays.py
@@ -1,17 +1,25 @@
-arr1_2d = [
+arr1 = [
     list(map(int, input().split(" ")))
     for _ in range(3)
 ]
 
 input()
 
-arr2_2d = [
+arr2 = [
     list(map(int, input().split(" ")))
+    for _ in range(3)
+]
+
+arr_3 = [
+    [0 for _ in range(3)]
     for _ in range(3)
 ]
 
 for i in range(3):
     for j in range(3):
-        print(arr1_2d[i][j] * arr2_2d[i][j], end = " ")
+        arr_3[i][j] = arr1[i][j] * arr2[i][j]
 
+for row in arr_3:
+    for elem in row:
+        print(elem, end=" ")
     print()
