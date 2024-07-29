2eb7e327970a62e7bc31435e3b15e94de61b3bbf
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-07-29 19:54:35 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240729/두 개의 격자 비교하기/compare-two-grid.py
+++ 240729/두 개의 격자 비교하기/compare-two-grid.py
@@ -11,12 +11,12 @@ arr2 = [
 ]
 
 arr3 = [
-    [0 for _ in range(m)]
-    for _ in range(n)
+    [0 for _ in range(n)]
+    for _ in range(m)
 ]
 
-for i in range(m):
-    for j in range(n):
+for i in range(n):
+    for j in range(m):
         if arr1[i][j] == arr2[i][j]:
             arr3[i][j] = 0
         else:
