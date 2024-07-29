18daa2a0f1b66b12e4156d73083c42f0f3033263
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-07-30 01:43:36 +0900

    Title: 공백을 기준으로 출력, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240730/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,14 @@
+str1 = input()
+str2 = input()
+
+for elem in str1:
+    if elem == '\n':
+        pass
+    else: 
+        print(elem, end="")
+
+for elem in str2:
+    if elem == '\n':
+        pass
+    else: 
+        print(elem, end="")
