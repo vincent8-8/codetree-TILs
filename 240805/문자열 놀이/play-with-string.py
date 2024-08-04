960d136b4d0666bd8389e2c2a611a71651e5b037
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-05 03:34:57 +0900

    Title: 문자열 놀이, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240805/문자열 놀이/play-with-string.py
+++ 240805/문자열 놀이/play-with-string.py
@@ -5,23 +5,23 @@ questions = [
     for _ in range(int(q))
 ]
 
+arr = list(s)
+
 for question in questions:
     if question[0] == '1':
-        tmp_arr = list(s)
         a = int(question[1]) - 1
         b = int(question[2]) - 1
 
-        tmp = tmp_arr[a]
-        tmp_arr[a] = tmp_arr[b]
-        tmp_arr[b] = tmp
-        print("".join(tmp_arr))
+        tmp = arr[a]
+        arr[a] = arr[b]
+        arr[b] = tmp
+        print("".join(arr))
 
     else:
         a = question[1]
         b = question[2]
-        tmp_arr = list(s)
 
-        for i in range(0, len(tmp_arr)):
-            if tmp_arr[i] == a:
-                tmp_arr[i] = b
-        print("".join(tmp_arr))
+        for i in range(0, len(arr)):
+            if arr[i] == a:
+                arr[i] = b
+        print("".join(arr))
