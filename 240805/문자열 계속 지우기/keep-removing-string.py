2098f5d65faa4138b4fe4409ef0fde71cca67ba0
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-05 23:46:25 +0900

    Title: 문자열 계속 지우기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/문자열 계속 지우기/keep-removing-string.py
+++ 240805/문자열 계속 지우기/keep-removing-string.py
@@ -2,9 +2,12 @@ a = input()
 b = input()
 
 index = a.find(b)
+length = len(b)
 
 while index != -1:
+    arr = list(a)
+    del arr[index : index + length]
+    a = "".join(arr)
     index = a.find(b)
-    a = a[0:index] + a[index+len(b):]
 
 print(a)
