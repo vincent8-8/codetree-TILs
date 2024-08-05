5f98fdf1c754800f07e9d5a48ca93c1a4d3df786
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-06 01:02:12 +0900

    Title: 소문자와 숫자, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/소문자와 숫자/letter-and-number.py
+++ 240806/소문자와 숫자/letter-and-number.py
@@ -3,5 +3,5 @@ s = input()
 for c in s:
     if (c >= 'A' and c <= 'Z') or (c >= 'a' and c <= 'z'):
         print(c.lower(), end="")
-    elif (ord(c) >= ord('1')) and (ord(c) <= ord('9')):
+    elif (ord(c) >= ord('0')) and (ord(c) <= ord('9')):
         print(c, end="")
