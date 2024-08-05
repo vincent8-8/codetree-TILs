072eb789ae2320afe04b634989e72a64a8d603d4
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-05 17:18:29 +0900

    Title: 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240805/문자 제거하기/del-char.py
+++ 240805/문자 제거하기/del-char.py
@@ -3,12 +3,15 @@ s = input()
 arr = list(s)
 n = len(s) - 1
 
-for _ in range(n):
-    index = int(input())
+if n == 0:
+    print(s)
+else:
+    for _ in range(n):
+        index = int(input())
 
-    if index < n:
-        arr.pop(index)
-        print("".join(arr))
-    else:
-        arr.pop(-1)
-        print("".join(arr))
+        if index < n:
+            arr.pop(index)
+            print("".join(arr))
+        else:
+            arr.pop(-1)
+            print("".join(arr))
