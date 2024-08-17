808e95cf1d63828ba818202c3133bcaecc3ec0ea
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-17 18:40:03 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240817/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240817/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,10 +1,11 @@
 l = int(input())
 _list = list(map(int, input().split()))
+step = min(_list)
 
 def f(n):
     for i in range(0, l):
         if n % _list[i] != 0:
-            return f(n + 1)
+            return f(n + step)
     
     return n
 
