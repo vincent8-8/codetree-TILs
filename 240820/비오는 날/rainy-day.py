bae916a27351ca39f65c0efeb7576b179f0ad501
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-20 13:30:31 +0900

    Title: 비오는 날, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240820/비오는 날/rainy-day.py
@@ -0,0 +1,32 @@
+n = int(input())
+_list = [
+    tuple(input().split())
+    for _ in range(n)
+]
+
+class Day:
+    def __init__(self, date="",  w_day="", weather=""):
+        self.date = date
+        self.w_day = w_day
+        self.weather = weather
+
+days = [Day() for _ in range(n)]
+r_days = []
+
+for i in range(n):
+    date, w_day, weather = _list[i]
+
+    days[i].date = date
+    days[i].w_day = w_day
+    days[i].weather = weather
+
+    if days[i].weather == "Rain":
+        r_days.append(days[i])
+
+index = 0
+
+for i in range(len(r_days) - 1):
+    if r_days[i].date < r_days[i + 1].date:
+        index = i
+
+print(r_days[index].date, r_days[index].w_day, r_days[index].weather)
