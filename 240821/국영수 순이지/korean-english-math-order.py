bd5bbc16a4c61da027b67e46f551c5ccbcbae74d
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-21 00:35:43 +0900

    Title: 국영수 순이지, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240821/국영수 순이지/korean-english-math-order.py
+++ 240821/국영수 순이지/korean-english-math-order.py
@@ -4,7 +4,7 @@ _list = [
     for _ in range(n)
 ]
 
-_list.sort(key=lambda x: (-x[1], -x[2], -x[3]))
+_list.sort(key=lambda x: (-int(x[1]), -int(x[2]), -int(x[3])))
 
 for name, kor, eng, math in _list:
     print(name, kor, eng, math)
