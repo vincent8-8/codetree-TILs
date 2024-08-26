e6cffeb472c918ad301dc2b4ebbce767121ce676
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-27 01:02:12 +0900

    Title: 진수 to 진수, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240827/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,20 @@
+a, b = map(int, input().split())
+n = input()
+
+digits_a = []
+digits_b = []
+
+num = 0
+for c in n:
+    num = num * a + int(c)
+
+while True:
+    if num < b:
+        digits_b.append(num)
+        break
+    
+    digits_b.append(num % b)
+    num //= b
+
+for digit in digits_b:
+    print(digit, end="")
