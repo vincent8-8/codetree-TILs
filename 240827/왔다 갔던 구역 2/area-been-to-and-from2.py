55283bfbe70284b9070ea83a1df43fd8b54c3cbb
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-27 13:11:42 +0900

    Title: 왔다 갔던 구역 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240827/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240827/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,15 +1,24 @@
 n = int(input())
 blocks = [0] * 1001
 start = 500
+pre = ""
 
 for _ in range(n):
     x, direction = input().split()
     
     if direction == "L":
+        if pre == "R":
+            blocks[start] -= 1
+
         for _ in range(int(x)):
-            blocks[start] += 1
+            blocks[start] += 1 
             start -= 1
+        pre = "L"
+
     else:
+        if pre == "L":
+            blocks[start] -= 1
+            
         for _ in range(int(x)):
             blocks[start] += 1
             start += 1
