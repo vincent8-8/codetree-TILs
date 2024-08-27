ae0a5fe77c3a2478eed1a35bc9930e5725e5504d
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-27 13:39:39 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240827/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,36 @@
+n = int(input())
+blocks = [ [0, 0, 0] for _ in range(1001)]
+start = 500
+
+for _ in range(n):
+    x, direction = input().split()
+    
+    if direction == "L":
+
+        for _ in range(int(x)):
+            blocks[start][0] = 1 
+            blocks[start][1] += 1
+            start -= 1
+        start += 1
+
+    else:
+
+        for _ in range(int(x)):
+            blocks[start][0] = 2 
+            blocks[start][2] += 1
+            start += 1
+        start -= 1
+
+cnt_gray = 0
+cnt_black = 0 
+cnt_white = 0
+
+for i in range(1001):
+    if blocks[i][1] >= 2 and blocks[i][2] >= 2:
+        cnt_gray += 1
+    elif blocks[i][0] == 1:
+        cnt_white += 1
+    elif blocks[i][0] == 2:
+        cnt_black += 1
+
+print(cnt_white, cnt_black, cnt_gray)
