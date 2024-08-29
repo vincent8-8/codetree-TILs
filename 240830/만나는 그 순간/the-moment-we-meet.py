ad9d3a7bac7723679480b8c7c51f0a9eadff6160
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-30 01:04:59 +0900

    Title: 만나는 그 순간, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240830/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,47 @@
+n, m = map(int, input().split())
+
+pos_A = []
+pos_B = []
+ 
+for i in range(n):
+    v, d = input().split()
+    
+    if v == "R":
+        for j in range(int(d)):
+            if not pos_A:
+                pos_A.append(1)
+            else:
+                pos_A.append(pos_A[-1] + 1)
+    elif v == "L":
+        for j in range(int(d)):
+            if not pos_A:
+                pos_A.append(-1)
+            else:
+                pos_A.append(pos_A[-1] - 1)
+
+for i in range(m):
+    v, d = input().split()
+    
+    if v == "R":
+        for j in range(int(d)):
+            if not pos_B:
+                pos_B.append(1)
+            else:
+                pos_B.append(pos_B[-1] + 1)
+    elif v == "L":
+        for j in range(int(d)):
+            if not pos_B:
+                pos_B.append(-1)
+            else:
+                pos_B.append(pos_B[-1] - 1)
+
+# print(pos_A, pos_B) # check
+
+cnt = -1
+
+while pos_A:
+    cnt += 1
+    if pos_A.pop(0) == pos_B.pop(0):
+        cnt += 1
+        print(cnt)
+        break
