197c4dfb1ba160b4d9ea1f662de053a29d6ccba3
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-29 10:22:49 +0900

    Title: 연속되는 수 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240829/연속되는 수 2/continuous-number2.py
+++ 240829/연속되는 수 2/continuous-number2.py
@@ -8,11 +8,10 @@ cnt = 0
 max_cnt = 0
 
 for i in range(n):
-    if i == 0 or _list[i] == _list[i - 1]:
-        cnt += 1
-    else:
+    if i != 0 and _list[i] != _list[i - 1]:
         if max_cnt < cnt:
             max_cnt = cnt
-            cnt = 1
+            cnt = 0
+    cnt += 1
     
 print(max_cnt)
