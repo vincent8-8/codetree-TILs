9cb85522bbbeff075d672212b06a95eebb3715ef
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-31 06:20:01 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240831/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,28 @@
+OFFSET = 64
+n, m = map(int, input().split())
+graph = [[0] * m for _ in range(n)]
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < m
+
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0] 
+dir_num = 0
+graph[0][0] = 'A'
+x, y = 0, 0
+
+i = 2
+
+while i <= n * m:
+    nx, ny = x + dxs[dir_num], y + dys[dir_num]
+
+    if in_range(nx, ny) and graph[nx][ny] == 0:
+        x, y = nx, ny
+        graph[x][y] = chr((i % 26) + OFFSET)
+        i += 1
+    else:
+        dir_num = (dir_num + 1) % 4
+
+for i in range(n):
+    for j in range(m):
+        print(graph[i][j], end=" ")
+    print()
