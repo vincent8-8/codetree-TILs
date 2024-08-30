74d69e65d24e3ae67283e66da73206c17180c0c6
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-30 14:52:08 +0900

    Title: 좌우로 움직이는 로봇, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240830/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240830/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,45 +1,42 @@
-MAX_R = 2000001
-OFFSET = 1000000
-
-pos_A = [0] * MAX_R
-pos_B = [0] * MAX_R
-curr_A = OFFSET
-curr_B = OFFSET
+pos_A = []
+pos_B = []
+curr_A = 0
+curr_B = 0
 
 # relative postion (which one is left? A or B)
 left = ""
 
 n, m = map(int, input().split())
 
-for i in range(1, n + 1):
+for i in range(0, n):
     t, d = input().split()
     t = int(t)
 
     if d == "R":
         curr_A += t
-        pos_A[curr_A] = i
     else:
         curr_A -= t
-        pos_A[curr_A] = i
+    
+    pos_A.append(curr_A)
 
-for i in range(1, m + 1):
+for j in range(0, m):
     t, d = input().split()
     t = int(t)
 
     if d == "R":
         curr_B += t
-        pos_B[curr_B] = i
     else:
         curr_B -= t
-        pos_B[curr_B] = i
+    
+    pos_B.append(curr_B)
 
-i, j = 1, 1
+i = 0
+j = 0
 cnt = 0
 
-while i != n or j != m:
-
-    A = pos_A.index(i)
-    B = pos_B.index(j)
+while (i < n) or (j < m):
+    A = pos_A[i]
+    B = pos_B[j]
 
     if A < B:
         if left == "B":
@@ -49,10 +46,15 @@ while i != n or j != m:
         if left == "A":
             cnt += 1
         left = "B"
+    
+    
+    if i >= n - 1 and j >= m - 1:
+        break
 
-    if i < n:
+    if i + 1 < n:
         i += 1
-    if j < m:
+    if j + 1 < m:
         j += 1
+
 
 print(cnt)
