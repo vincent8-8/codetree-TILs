bbc13729a49b53d4f4e389eb2120a0639eb9f01f
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-30 14:05:49 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240830/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240830/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,70 +1,58 @@
+MAX_R = 2000001
+OFFSET = 1000000
+
+pos_A = [0] * MAX_R
+pos_B = [0] * MAX_R
+curr_A = OFFSET
+curr_B = OFFSET
+
+# relative postion (which one is left? A or B)
+left = ""
+
 n, m = map(int, input().split())
-A_list = []
-B_list = []
 
-for _ in range(n):
+for i in range(1, n + 1):
     t, d = input().split()
     t = int(t)
 
-    if not A_list:
-        if d == "R":
-            A_list.append(1)
-            for _ in range(t - 1):
-                A_list.append(A_list[-1] + 1)
-        else:
-            A_list.append(-1)
-            for _ in range(t - 1):
-                A_list.append(A_list[-1] - 1)
+    if d == "R":
+        curr_A += t
+        pos_A[curr_A] = i
     else:
-        if d == "R":
-            for _ in range(t):
-                A_list.append(A_list[-1] + 1)
-        else:
-            for _ in range(t):
-                A_list.append(A_list[-1] - 1)
+        curr_A -= t
+        pos_A[curr_A] = i
 
-for _ in range(m):
+for i in range(1, m + 1):
     t, d = input().split()
     t = int(t)
 
-    if not B_list:
-        if d == "R":
-            B_list.append(1)
-            for _ in range(t - 1):
-                B_list.append(B_list[-1] + 1)
-        else:
-            B_list.append(-1)
-            for _ in range(t - 1):
-                B_list.append(B_list[-1] - 1)
+    if d == "R":
+        curr_B += t
+        pos_B[curr_B] = i
     else:
-        if d == "R":
-            for _ in range(t):
-                B_list.append(B_list[-1] + 1)
-        else:
-            for _ in range(t):
-                B_list.append(B_list[-1] - 1)
+        curr_B -= t
+        pos_B[curr_B] = i
 
-# init with random number
-prev_A = A_list[0]
-prev_B = B_list[0]
-
-curr_A = 0
-curr_B = 0
-
+i, j = 1, 1
 cnt = 0
 
-while A_list or B_list:
-    if A_list:
-        curr_A = A_list.pop(0)
+while i != n or j != m:
 
-    if B_list:
-        curr_B = B_list.pop(0)
-    
-    if curr_A == curr_B:
-        if prev_A != prev_B:
+    A = pos_A.index(i)
+    B = pos_B.index(j)
+
+    if A < B:
+        if left == "B":
             cnt += 1
-            
-    prev_A = curr_A
-    prev_B = curr_B
+        left = "A"
+    elif A > B:
+        if left == "A":
+            cnt += 1
+        left = "B"
+
+    if i < n:
+        i += 1
+    if j < m:
+        j += 1
 
 print(cnt)
