d21814d2b494fb48e33be0fcaa83cfbcaec94e3c
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-08-31 06:06:27 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 240831/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
+++ 240831/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 graph = [[0] * m for _ in range(n)]
 
 def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
+    return 0 <= x and x < n and 0 <= y and y < m
 
 dxs, dys = [1, 0, -1, 0], [0, 1, 0, -1] 
 dir_num = 0
