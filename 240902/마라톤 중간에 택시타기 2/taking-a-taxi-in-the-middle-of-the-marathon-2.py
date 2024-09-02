cbf087fd8ffbb7972b2db0b9e9729c9ebee81a63
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-02 14:37:48 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240902/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,28 @@
+n = int(input())
+_list = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+max_diff  = -1
+target_index = -1
+
+for i in range(1, n - 1):
+    before = abs(_list[i - 1][0] - _list[i][0]) + abs(_list[i - 1][1] - _list[i][1]) + abs(_list[i][0] - _list[i + 1][0]) + abs(_list[i][1] - _list[i + 1][1])
+    after = abs(_list[i - 1][0] - _list[i + 1][0]) + abs(_list[i - 1][1] - _list[i + 1][1])
+    
+    diff = before - after
+
+    if diff > max_diff:
+        max_diff = diff
+        target_index = i
+    
+result_distance = 0
+
+for i in range(n - 1):
+    if (i + 1) != target_index:
+        result_distance += abs(_list[i][0] - _list[i + 1][0]) + abs(_list[i][1] - _list[i + 1][1])
+    else:
+        result_distance += abs(_list[i - 1][0] - _list[i + 1][0]) + abs(_list[i - 1][1] - _list[i + 1][1])
+
+print(result_distance)
