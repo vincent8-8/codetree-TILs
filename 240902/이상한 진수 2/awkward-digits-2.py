36bcbb8e5156ff13429772e22f498eeea9772f78
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-02 13:24:40 +0900

    Title: 이상한 진수 2, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240902/이상한 진수 2/awkward-digits-2.py
+++ 240902/이상한 진수 2/awkward-digits-2.py
@@ -2,10 +2,13 @@ digits = list(input())
 len_digits = len(digits)
 answer = 0
 
-for i in range(len_digits):
-    if digits[i] == "0":
-        digits[i] = "1"
-        break
+if "0" in digits:
+    for i in range(len_digits):
+        if digits[i] == "0":
+            digits[i] = "1"
+            break
+else:
+    digits[-1] = 0
 
 digits = digits[::-1]
 
