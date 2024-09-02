635d5aecde36825c04b2987bb183bc078cc12c9b
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-02 13:04:08 +0900

    Title: 체크판위에서 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240902/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,18 @@
+r, c = map(int, input().split())
+_list = [
+    list(input().split())
+    for _ in range(r)
+]
+
+curr_color = _list[0][0]
+cnt = 0
+
+for i in range(1, r):
+    for j in range(1, c):
+        for k in range(i + 1, r):
+            for l in range(j + 1, c):
+                if _list[i][j] != curr_color:
+                    if _list[k][l] == curr_color:
+                        cnt += 1
+                        break
+print(cnt)
