f524ee76259b723c2c10d7e1dee15d04b639a7fb
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-02 13:12:35 +0900

    Title: 체크판위에서 2, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240902/체크판위에서 2/on-the-checkboard-2.py
+++ 240902/체크판위에서 2/on-the-checkboard-2.py
@@ -4,17 +4,16 @@ _list = [
     for _ in range(r)
 ]
 
-curr_color = _list[0][0]
+start_color = _list[0][0]
 cnt = 0
 
 for i in range(1, r):
     for j in range(1, c):
         for k in range(i + 1, r):
             for l in range(j + 1, c):
-                if _list[i][j] != curr_color:
-                    if _list[k][l] == curr_color:
+                if _list[i][j] != start_color and _list[k][l] == start_color:
+                    if k != (r - 1) and l != (c - 1):
                         cnt += 1
-                        break
 if _list[0][0] == _list[r - 1][c - 1]:
     print("0")
 else:
