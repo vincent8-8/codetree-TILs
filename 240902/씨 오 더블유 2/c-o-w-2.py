70793b9e4446bc2e762ae650dcdbfc21109776c2
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-02 21:07:47 +0900

    Title: 씨 오 더블유 2, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240902/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,14 @@
+n = int(input())
+s = input()
+
+cnt = 0
+
+for i in range(n):
+    if s[i] == "C":
+        for j in range(i + 1, n):
+            if s[j] == "O":
+                for k in range(j + 1, n):
+                    if s[k] == "W":
+                        cnt += 1
+
+print(cnt)
