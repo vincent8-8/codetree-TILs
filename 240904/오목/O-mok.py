a1f447675b4601d1f78c70e17aee90f341dadda0
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-04 15:17:55 +0900

    Title: 오목, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240904/오목/O-mok.py
+++ 240904/오목/O-mok.py
@@ -52,6 +52,18 @@ for i in range(19):
                     print(find_answer)
                     print(i + 3, j + 1)
 
+            if in_range(i + 1, j - 1) and _list[i + 1][j - 1] == 1:
+                for k in range(2, 5):
+                    if in_range(i + k, j - k) and _list[i + k][j - k] == 1:
+                        find_answer = 1
+                    else:
+                        find_answer = 0
+                        break
+
+                if find_answer:
+                    print(find_answer)
+                    print(i + 3, j - 1)
+
         elif _list[i][j] == 2:
             if in_range(i, j + 1) and _list[i][j + 1] == 2:
                 for k in range(2, 5):
@@ -88,6 +100,18 @@ for i in range(19):
                 if find_answer:
                     print(find_answer)
                     print(i + 3, j + 1)
+
+            if in_range(i + 1, j - 1) and _list[i + 1][j - 1] == 2:
+                for k in range(2, 5):
+                    if in_range(i + k, j - k) and _list[i + k][j - k] == 2:
+                        find_answer = 2
+                    else:
+                        find_answer = 0
+                        break
+
+                if find_answer:
+                    print(find_answer)
+                    print(i + 3, j - 1)
 
 if not find_answer:
     print("0")
