cac90de298579c5cace222a463abff5bc8c78a54
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-04 15:24:43 +0900

    Title: 오목, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240904/오목/O-mok.py
+++ 240904/오목/O-mok.py
@@ -1,117 +1,42 @@
-def in_range(x, y):
-    return 0 <= x and x < 19 and 0 <= y and y < 19
+import sys
 
-_list = [
+# 변수 선언 및 입력
+arr = [
     list(map(int, input().split()))
     for _ in range(19)
 ]
 
-find_answer = 0
+def in_range(x, y):
+    return 0 <= x and x < 19 and 0 <= y and y < 19
 
+dxs, dys = [1, 1, 1, -1, -1, -1, 0, 0], [-1, 0, 1, -1, 0, 1, -1, 1]
+
+# 모든 좌표에서 다 확인해봅니다.
 for i in range(19):
-    if find_answer:
-        break
-    for j in range(19):
-        if find_answer:
-            break
+	# 격자를 벗어나지 않을 범위로만 잡습니다.
+	for j in range(19):
+		
+		if arr[i][j] == 0:
+			continue
+		
+		for dx, dy in zip(dxs, dys):
+			curt = 1
+			curx = i
+			cury = j
+			while True:
+				nx = curx + dx
+				ny = cury + dy
+				if in_range(nx, ny) == False:
+					break
+				if arr[nx][ny] != arr[i][j]:
+					break
+				curt += 1
+				curx = nx
+				cury = ny
+			
+			if curt == 5:
+				print(arr[i][j])
+				print(i + 2 * dx + 1, j + 2 * dy + 1)
+				sys.exit()
 
-        if _list[i][j] == 1:
-            if in_range(i, j + 1) and _list[i][j + 1] == 1:
-                for k in range(2, 5):
-                    if in_range(i, j + k) and _list[i][j + k] == 1:
-                        find_answer = 1
-                    else:
-                        find_answer = 0
-                        break
-
-                if find_answer:
-                    print(find_answer)
-                    print(i + 1, j + 3)
-
-            if in_range(i + 1, j + 1) and _list[i + 1][j + 1] == 1:
-                for k in range(2, 5):
-                    if in_range(i + k, j + k) and _list[i + k][j + k] == 1:
-                        find_answer = 1
-                    else:
-                        find_answer = 0
-                        break
-
-                if find_answer:
-                    print(find_answer)
-                    print(i + 3, j + 3)
-
-            if in_range(i + 1, j) and _list[i + 1][j] == 1:
-                for k in range(2, 5):
-                    if in_range(i + k, j) and _list[i + k][j] == 1:
-                        find_answer = 1
-                    else:
-                        find_answer = 0
-                        break
-
-                if find_answer:
-                    print(find_answer)
-                    print(i + 3, j + 1)
-
-            if in_range(i + 1, j - 1) and _list[i + 1][j - 1] == 1:
-                for k in range(2, 5):
-                    if in_range(i + k, j - k) and _list[i + k][j - k] == 1:
-                        find_answer = 1
-                    else:
-                        find_answer = 0
-                        break
-
-                if find_answer:
-                    print(find_answer)
-                    print(i + 3, j - 1)
-
-        elif _list[i][j] == 2:
-            if in_range(i, j + 1) and _list[i][j + 1] == 2:
-                for k in range(2, 5):
-                    if in_range(i, j + k) and _list[i][j + k] == 2:
-                        find_answer = 2
-                    else:
-                        find_answer = 0
-                        break
-
-                if find_answer:
-                    print(find_answer)
-                    print(i + 1, j + 3)
-
-            if in_range(i + 1, j + 1) and _list[i + 1][j + 1] == 2:
-                for k in range(2, 5):
-                    if in_range(i + k, j + k) and _list[i + k][j + k] == 2:
-                        find_answer = 2
-                    else:
-                        find_answer = 0
-                        break
-
-                if find_answer:
-                    print(find_answer)
-                    print(i + 3, j + 3)
-
-            if in_range(i + 1, j) and _list[i + 1][j] == 2:
-                for k in range(2, 5):
-                    if in_range(i + k, j) and _list[i + k][j] == 2:
-                        find_answer = 2
-                    else:
-                        find_answer = 0
-                        break
-
-                if find_answer:
-                    print(find_answer)
-                    print(i + 3, j + 1)
-
-            if in_range(i + 1, j - 1) and _list[i + 1][j - 1] == 2:
-                for k in range(2, 5):
-                    if in_range(i + k, j - k) and _list[i + k][j - k] == 2:
-                        find_answer = 2
-                    else:
-                        find_answer = 0
-                        break
-
-                if find_answer:
-                    print(find_answer)
-                    print(i + 3, j - 1)
-
-if not find_answer:
-    print("0")
+print(0)
