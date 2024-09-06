09ba506b032d56e6e5a495e84e9789d81e93a28b
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-06 17:49:08 +0900

    Title: G or H 3, Time: 212ms, Memory: 25MB, Status: Passed - CodeTree

--- 240906/G or H 3/G-or-H-3.py
+++ 240906/G or H 3/G-or-H-3.py
@@ -1,18 +1,25 @@
-n, k = map(int, input().split())
-_list = list("" for _ in range(10000))
-max_score = 0
+MAX_NUM = 10000
+
+# 변수 선언 및 입력
+n, k = tuple(map(int, input().split()))
+arr = [0] * (MAX_NUM + 1)
 
 for _ in range(n):
-    idx, alph = input().split()
-    _list[int(idx) - 1] = alph
+    x, c = tuple(input().split())
+    x = int(x)
+    
+    arr[x] = 1 if c == 'G' else 2
 
-for i in range(10000 - k):
-    score = 0
+
+# 모든 구간의 시작점을 잡아봅니다.
+max_sum = 0
+for i in range(MAX_NUM - k + 1):
+    # 해당 구간 내 원소의 합을 구합니다.
+    sum_interval = 0
     for j in range(i, i + k + 1):
-        if _list[j] == "G":
-            score += 1
-        elif _list[j] == "H":
-            score += 2
-    max_score = max(max_score, score)
+        sum_interval += arr[j]
 
-print(max_score)
+    # 최댓값을 구합니다.
+    max_sum = max(max_sum, sum_interval)
+                        
+print(max_sum)
