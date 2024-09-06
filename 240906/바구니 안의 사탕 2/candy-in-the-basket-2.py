d140557f62185cd9ac5000c12596ca408c438e88
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-06 23:45:12 +0900

    Title: 바구니 안의 사탕 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240906/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240906/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -6,13 +6,29 @@ for _ in range(n):
     candies, idx = map(int, input().split())
     _list[idx] = candies
  
-for i in range(k, 101 - k):
+for i in range(0, 101):
     cnt = 0
-    for j in range(i - k, i + k + 1):
-        if _list[j] == 0:
-            continue
-        
+    if k * 2 > 101:
+        cnt = sum(_list)
+
+    elif i - k < 0:
+        for j in range(0, i + k + 1):
+            if _list[j] == 0:
+                continue
         cnt += _list[j]
+
+    elif i + k > 100:
+        for j in range(i - k, 101):
+            if _list[j] == 0:
+                continue
+        cnt += _list[j]
+        
+    else:
+        for j in range(i, i + (2 * k) + 1):
+            if _list[j] == 0:
+                continue
+        cnt += _list[j]
+
     max_cnt = max(max_cnt, cnt)
 
 print(max_cnt)
