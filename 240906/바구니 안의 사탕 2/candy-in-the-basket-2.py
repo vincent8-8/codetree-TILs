13bd42cae9f96fe63c77e00c563d4cec5e3c158e
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-06 23:20:33 +0900

    Title: 바구니 안의 사탕 2, Time: 122ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240906/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240906/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,12 +1,12 @@
 n, k = map(int, input().split())
-_list = [0] * 100
+_list = [0] * 101
 max_cnt = 0
 
 for _ in range(n):
     candies, idx = map(int, input().split())
     _list[idx - 1] = candies
  
-for i in range(k, 100 - k):
+for i in range(k, 101 - k):
     cnt = 0
     for j in range(i - k, i + k + 1):
         if _list[j] == 0:
