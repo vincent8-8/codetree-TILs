c5f3a769fdd025922687d65caa1970052de0a34b
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-06 23:02:42 +0900

    Title: 아름다운 수열 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240906/아름다운 수열 2/beautiful-sequence-2.py
+++ 240906/아름다운 수열 2/beautiful-sequence-2.py
@@ -4,15 +4,9 @@ B_list = list(map(int, input().split()))
 cnt = 0
 
 for i in range(n - m + 1):
-    is_ans = True
     tmp_list = A_list[i : i + m]
 
-    for j in range(m):
-        if not B_list[j] in tmp_list:
-            is_ans = False
-            break
-    
-    if is_ans:
+    if sorted(tmp_list) == B_list:
         cnt += 1
 
 print(cnt)
