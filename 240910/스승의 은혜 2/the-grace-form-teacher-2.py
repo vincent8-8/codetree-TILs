eed084b3b65e5fb7c2f6c5b380e0117a1d717cb8
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-10 14:02:00 +0900

    Title: 스승의 은혜 2, Time: 225ms, Memory: 25MB, Status: Passed - CodeTree

--- 240910/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240910/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -5,14 +5,14 @@ _list = [
 ]
 
 max_num = 0
-local_sum = 0
 
 for i in range(n):
     _list[i] //= 2
-    _list.sort()
+    list_2 = sorted(_list)
     cnt = 0
+    local_sum = 0
     for j in range(n):
-        local_sum += _list[j]
+        local_sum += list_2[j]
 
         if local_sum <= b:
             cnt += 1
