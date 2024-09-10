2a17ca4bb827222711e0aa7a80e8e717232fe635
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-10 13:49:05 +0900

    Title: 스승의 은혜 2, Time: 125ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240910/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,22 @@
+n, b = map(int, input().split())
+_list = [
+    int(input())
+    for _ in range(n)
+]
+
+_list.sort()
+max_num = 0
+local_sum = 0
+
+for i in range(n):
+    _list[i] //= 2
+    cnt = 0
+    for j in range(n):
+        if local_sum >= b:
+            break
+        local_sum += _list[j]
+        cnt += 1
+    
+    max_num = max(max_num, cnt)
+
+print(max_num)
