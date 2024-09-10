a9da5d813a37ef7d30e175fe4a32b364ebeaed46
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-10 11:27:42 +0900

    Title: 겹치지 않는 선분 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240910/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240910/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -13,21 +13,22 @@ cnt = 0
 for i in range(n):
     is_answer = True
 
+    std_start, std_end = _list[i]
+    std_start += OFFSET
+    std_end += OFFSET
+
     for j in range(n):
         if i == j:
             continue
-        
-        std_start, std_end = _list[i]
-        std_start += OFFSET
-        std_end += OFFSET
 
         counter_start = _list[j][0] + OFFSET
         counter_end = _list[j][1] + OFFSET
 
-        if std_start < counter_start < std_end and std_start <  counter_end < std_end:
+        if counter_start <= std_start <= counter_end and counter_start <= std_end <= counter_end:
             is_answer = False
             break
-        elif counter_start < std_start < counter_end and counter_start < std_end < counter_end:
+        
+        if std_start <= counter_start and counter_end <= std_end:
             is_answer = False
             break
 
