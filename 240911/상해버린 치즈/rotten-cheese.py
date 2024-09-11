c172b66fb1a66346e0d7937ecf96c1ce2ba4316d
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-11 21:09:56 +0900

    Title: 상해버린 치즈, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240911/상해버린 치즈/rotten-cheese.py
+++ 240911/상해버린 치즈/rotten-cheese.py
@@ -24,4 +24,39 @@ for i in range(s):
         if _info[j][0] == people and _info[j][2] >= time:
             cheese[_info[j][1] - 1] = 0
 
-print(cheese)
+# 둘 중 한명이 먹지 않았다면 제외
+for i in range(m):
+    if cheese[i] == 0:
+        continue
+
+    did_everyone_ate = True
+    
+    for j in range(s):
+        people, time = _hurt_time[j]
+        did_everyone_ate = False
+
+        for k in range(d):
+            if _info[k][0] == people and _info[k][1] == (i + 1):
+                did_everyone_ate = True
+        if not did_everyone_ate:
+            cheese[i] = 0
+            break
+
+max_cnt = 0
+
+for i in range(m):
+    if cheese[i] == 0:
+        continue
+    
+    cnt = 0
+
+    for j in range(d):
+        peoples = []
+
+        if _info[j][1] == (i + 1):
+            if _info[j][0] not in peoples: 
+                cnt += 1
+                peoples.append(_info[j][0])
+    
+    max_cnt = max(max_cnt, cnt)
+print(max_cnt)
