704abae1f4d4c0433cac947f9ee770e31ed64ee5
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-12 10:35:18 +0900

    Title: 스승의 은혜 3, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240912/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,30 @@
+n, b = map(int, input().split())
+_list = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+max_cnt = 0
+
+for i in range(n):
+    total_prices = []
+    local_sum = 0
+    cnt = 0
+
+    for j in range(n):
+        if i == j:
+            _list[i][0] //= 2
+        
+        total_prices.append(_list[j][0] + _list[j][1])
+
+    total_prices.sort()
+
+    for k in range(n):
+        local_sum += total_prices[k]
+        if local_sum >= b:
+            break
+        cnt += 1
+
+    max_cnt = max(max_cnt, cnt)
+    _list[i][0] *= 2
+
+print(max_cnt)
