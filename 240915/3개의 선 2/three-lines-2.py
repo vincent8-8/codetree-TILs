6f826d73bf6f2623542301ac31c228c14d798bf0
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-15 00:36:59 +0900

    Title: 3개의 선 2, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240915/3개의 선 2/three-lines-2.py
@@ -0,0 +1,54 @@
+n = int(input())
+_list = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+is_answer = 0
+
+def check_answer_x(x1, y1, x2):
+    for x, y in _list:
+        if not ( x in (x1, x2) or (y == y1) ):
+            return 0
+    return 1
+
+def check_answer_y(x1, y1, y2):
+    for x, y in _list:
+        if not ( (x == x1) or y in (y1, y2) ):
+            return 0
+    return 1
+
+def check_answer_all_x(x1, x2, x3):
+    for x, y in _list:
+        if not x in (x1, x2, x3):
+            return 0
+    return 1
+
+def check_answer_all_y(y1, y2, y3):
+    for x, y in _list:
+        if not x in (y1, y2, y3):
+            return 0
+    return 1
+
+
+for i in range(11):
+    for j in range(11):
+        if i == j:
+            continue
+
+        for k in range(11):
+            if i == k or j == k:
+                continue
+
+            if is_answer == 1:
+                break
+
+            is_answer = max(check_answer_x(i, j, k), check_answer_y(i, j, k), check_answer_all_x(i, j, k), check_answer_all_y(i, j, k))
+        
+        if is_answer == 1:
+            break
+
+    if is_answer == 1:
+        break
+
+print(is_answer)
