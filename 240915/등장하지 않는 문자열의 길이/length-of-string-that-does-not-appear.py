9af4b116c097575357c2d4ce9b19a5edc0ed5861
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-15 16:03:30 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240915/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240915/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -3,14 +3,18 @@ s = input()
 
 min_len = n
 
+def search(length):
+    for i in range(n - length):
+        target = s[i : i + length]
+        
+        if ( target in s[0:i] ) or ( target in s[i + 1:] ):
+            return False
+
+    return True 
+
 for i in range(1, n):
-    local_len = 101
-    # 문자열 설정
-    target = s[0:i]
-    if target in s[i:]:
-        continue
-    
-    min_len = i
-    break
+    if search(i):
+        min_len = i
+        break
 
 print(min_len)
