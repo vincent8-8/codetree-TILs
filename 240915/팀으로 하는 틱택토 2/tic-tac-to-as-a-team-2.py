1d8928fb4ae60c9325252f42bb8435cb27b6b3a5
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-15 15:25:14 +0900

    Title: 팀으로 하는 틱택토 2, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240915/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240915/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,12 +1,22 @@
 _graph = [
-    list(map(int, input().split()))
+    input()
     for _ in range(3)
 ]
+
 cnt = 0
 sets = []
 
-if len(set())
-
+for i in range(3):
+    sets.append(set((_graph[i][0], _graph[i][1], _graph[i][2])))
+    sets.append(set((_graph[0][i], _graph[1][i], _graph[2][i])))
         
-   
-print(cnt)
+sets.append(set((_graph[0][0], _graph[1][1], _graph[2][2])))
+sets.append(set((_graph[2][2], _graph[1][1], _graph[2][0])))
+
+answer_list = []
+
+for i in range(len(sets)):
+    if len(sets[i]) == 2 and not(sets[i] in answer_list):
+        answer_list.append(sets[i])
+
+print(len(answer_list))
