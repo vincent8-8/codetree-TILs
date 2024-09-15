7f011cf4e96a2a0dca5c938df544fbb07b12c94c
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-16 01:22:57 +0900

    Title: 최대 최소간의 차, Time: 185ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240916/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 240916/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -5,7 +5,7 @@ avg = round(sum(_list) / n)
 cost = 0
 
 for i in range(n):
-    for j in range(i + 1, n):
+    for j in range(n):
         if abs(_list[i] - _list[j]) <= k:
             continue
         
