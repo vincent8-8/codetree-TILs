e072312d145c7962c52bc9664f6c326767842adf
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-16 01:52:37 +0900

    Title: 최대 최소간의 차, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240916/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 240916/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -1,26 +1,25 @@
 n, k = map(int, input().split())
 _list = list(map(int, input().split()))
+_list.sort()
 
-avg = sum(_list) / n
+avg = round(sum(_list) / n)
+k_2 = round(k / 2)
 cost = 0
 
-for i in range(n):
-    for j in range(i + 1, n):
-        if abs(_list[i] - _list[j]) <= k:
-            continue
-        
-        while abs(_list[i] - _list[j]) > k:
-            if abs(_list[i] - avg) > abs(_list[j] - avg):
-                if _list[i] - avg > 0:
-                    _list[i] -= 1
-                else:
-                    _list[i] += 1
-                
-            else:
-                if _list[j] - avg > 0:
-                    _list[j] -= 1
-                else:
-                    _list[j] += 1
-                
+for i in range(n // 2):
+    while not (avg - k_2) <= _list[i] <= (avg + k_2):
+        if (avg - k_2) > _list[i]:
+            _list[i] += 1
             cost += 1
+        else:
+            _list[i] -= 1
+            cost += 1
+
+_list.sort()
+
+for j in range(-1, -(n // 2 + 1), -1):
+    while _list[j] - _list[0] > k:
+        _list[j] -= 1
+        cost += 1
+
 print(cost)
