3debb52dfc758a5474b9cea6b01fbc2bcce8cae0
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-09-19 18:27:16 +0900

    Title: A, B, C, D 찾기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240919/A, B, C, D 찾기/find-a-b-c-d.py
+++ 240919/A, B, C, D 찾기/find-a-b-c-d.py
@@ -5,17 +5,9 @@ c, d = 0, 0
 
 if (a + b) != _list[2]:
     c = _list[2]
-    _list.remove(a + b)
-    _list.remove(a + c)
-    _list.remove(b + c)
-
-    d = _list[3]
 else:
     c = _list[3]
-    _list.remove(a + b)
-    _list.remove(a + c)
-    _list.remove(b + c)
 
-    d = _list[4]
+d = _list[-1] - c - b - a
 
 print(a, b, c, d)
