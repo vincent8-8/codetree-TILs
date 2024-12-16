80ab0052201b8c4e0419f8a8c76c518b2dbb3c07
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-12-16 17:51:01 +0900

    Title: A, B, C, D 찾기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241216/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 241216/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -3,25 +3,22 @@ _list = list(map(int, input().split()))
 _list.sort()
 length = len(_list)
 a = _list[0]
-b = 0
+b = _list[1]
 c = 0
 d = 0
 
 def find_answer(a, b, c, d):
     if (a + b + c + d) in _list:
-        if (a + b + c) in _list:
-            if (a + b) in _list:
+        if (a + b + c) in _list and (b + c + d) in _list: 
+            if (c + d) in _list:
                 return True
+    return False
 
-for i in range(1, length//2):
-    b = _list[i]
-    for j in range(2, length//2):
-        c = _list[j]
-        for k in range(3, length//2):
-            d = _list[k]
-            found = find_answer(a, b, c, d)
-            if found:
-                break
+for i in range(2, length//2):
+    c = _list[i]
+    for j in range(3, length//2):
+        d = _list[j]
+        found = find_answer(a, b, c, d)
         if found:
             break
     if found:
