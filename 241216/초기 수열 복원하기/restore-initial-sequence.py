5e9489a515ae1e75ae5fa7dd10f046ec56c4467c
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-12-16 19:58:20 +0900

    Title: 초기 수열 복원하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241216/초기 수열 복원하기/restore-initial-sequence.py
@@ -0,0 +1,21 @@
+n = int(input())
+_list = list(map(int, input().split()))
+
+# 중복을 찾는 방법은 리스트를 set으로 변환하는 방법을 사용해 길이가 n-1 이 맞는지 확인한다.
+
+for start in range(1, _list[0]):
+    answer = [start]
+
+    for i in range(n-1):
+        answer.append(_list[i] - answer[-1])
+
+    answer_set = set(answer)
+    answer_set = sorted(answer_set)
+
+    if answer_set[0] <= 0:
+        continue
+    elif len(answer_set) != n:
+        continue
+    
+for elem in answer:
+    print(elem, end=" ")
