ff105b33518bc37261a3e362667bf857eb16c2eb
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-12-21 00:46:33 +0900

    Title: 이상한 폭탄 3, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241221/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,34 @@
+n, k = map(int, input().split())
+_list = [int(input()) for _ in range(n)]
+max_count = 0
+max_num = 0
+
+
+for standard in _list:
+    if standard == -1:
+        continue
+
+    same_num_list = []
+    count = 0
+
+    for i, elem in enumerate(_list):
+        if elem == standard:
+            same_num_list.append(i)
+            _list[i] = -1
+    
+    for j in range(len(same_num_list) - 1):
+        if (same_num_list[j + 1] - same_num_list[j]) < k:
+            count += 1
+        
+    if max_count == count:
+        max_num = standard
+    elif max_count < count:
+        max_count = count
+        max_num = standard
+    else:
+        continue
+
+if max_count == 0:
+    print(0)
+else:
+    print(max_num)
