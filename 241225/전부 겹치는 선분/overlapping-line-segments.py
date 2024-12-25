45b54158b59e99419b939c1a57a6a76da3eecfd3
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-12-25 20:59:38 +0900

    Title: 전부 겹치는 선분, Time: 75ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241225/전부 겹치는 선분/overlapping-line-segments.py
@@ -0,0 +1,13 @@
+n = int(input())
+_list = [list(map(int, input().split())) for _ in range(n)]
+
+_graph = [0] * 100
+
+for line in _list:
+    for i in range(line[0], line[1] + 1):
+        _graph[i] += 1
+
+if n in _graph:
+    print("Yes")
+else:
+    print("No")
