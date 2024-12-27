b057ec3443f446a3ecf300c47447a7ce767f3b0f
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-12-27 23:02:17 +0900

    Title: 코딩톡, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241227/코딩톡/coding-talk.py
@@ -0,0 +1,14 @@
+n, m, p = map(int, input().split())
+_messages = [list(input().split()) for _ in range(m)]
+_people = []
+
+for i in range(n):
+    _people.append(chr(i + 65))
+
+for i in range(p - 1, m):
+    person = _messages[i][0]
+    if person in _people:
+        _people.remove(person)
+
+for person in _people:
+    print(person, end=" ")
