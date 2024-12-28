70731c8116dfdbebdf85cbbb98108624b29fdad6
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-12-28 20:05:24 +0900

    Title: X 달리기, Time: 60ms, Memory: 17MB, Status: Passed - CodeTree

--- 241228/X 달리기/x-run.py
+++ 241228/X 달리기/x-run.py
@@ -1,24 +1,29 @@
 distance = int(input())
-time = 1
+min_time = distance
 
-while True:
+for time in range(1, distance):
     d = 0
+    extra_time = 0
 
     for i in range(1, time // 2 + 1):
         d += i * 2
-    
+
     if time % 2 != 0:
         d += time // 2 + 1
 
+
+    if d < distance:
+        remaining_distance = distance - d
+
+        for i in range(time // 2, 0, -1):
+            cut = remaining_distance // i
+
+            if cut >= 1:
+                remaining_distance -= cut * i
+                d += cut * i
+                extra_time += cut
+
     if d == distance:
-        break
-    elif d == (distance - 1):
-        time += 1
-        break
+        min_time = min(min_time, time + extra_time)
 
-    if time > distance:
-        break
-
-    time += 1
-
-print(time)
+print(min_time)
