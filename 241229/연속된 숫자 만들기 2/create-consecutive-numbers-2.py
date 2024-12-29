c7e815b22857f10577eb93b21a9890580e6aa4d0
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-12-29 20:11:43 +0900

    Title: 연속된 숫자 만들기 2, Time: 5066ms, Memory: 18MB, Status: Time Limit Exceed - CodeTree

--- 241229/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 241229/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -2,15 +2,20 @@ _list = list(map(int, input().split()))
 diff = max(_list) - min(_list)
 cost = 0
 
-while not diff == 2:
-    cost += 1
-    _list.sort()
-
-    if _list[1] - _list[0] < _list[2] - _list[1]:
+if _list[1] - _list[0] < _list[2] - _list[1]:
+    while not diff == 2:
+        cost += 1
+        _list.sort()
         _list[2] =  ((_list[0] + _list[1]) // 2) if (_list[0] + _list[1]) % 2 == 0 else ((_list[0] + _list[1]) // 2 + 1) 
-    else:
+        
+        diff = max(_list) - min(_list)
+
+else:
+    while not diff == 2:
+        cost += 1
+        _list.sort()
         _list[0] =  ((_list[1] + _list[2]) // 2) if (_list[1] + _list[2]) % 2 == 0 else ((_list[1] + _list[2]) // 2 + 1) 
 
-    diff = max(_list) - min(_list)
+        diff = max(_list) - min(_list)
 
 print(cost)
