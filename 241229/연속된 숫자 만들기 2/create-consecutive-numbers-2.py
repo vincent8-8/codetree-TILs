e35bd1fe8e6d80da90d16920af1894aa55da6635
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-12-29 20:54:40 +0900

    Title: 연속된 숫자 만들기 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241229/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 241229/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,40 +1,10 @@
 _list = list(map(int, input().split()))
+diff_a_b = _list[1] - _list[0]
+diff_b_c = _list[2] - _list[1]
 
-cost = 0
-
-while True:
-    diff_a_b = _list[1] - _list[0]
-    diff_b_c = _list[2] - _list[1]
-
-    if diff_a_b == 1 and diff_b_c == 1:
-        break
-
-    if diff_a_b < diff_b_c:
-        cost += 1
-
-        if diff_a_b == 1:
-            if (_list[1] + _list[2]) % 2 == 0:
-                _list[0] = (_list[1] + _list[2]) // 2
-            else:
-                _list[0] = (_list[1] + _list[2]) // 2 + 1
-        else:
-            if (_list[0] + _list[1]) % 2 == 0:
-                _list[2] = (_list[0] + _list[1]) // 2
-            else:
-                _list[2] = (_list[0] + _list[1]) // 2 + 1
-    else:
-        cost += 1
-        if diff_b_c == 1:
-            if (_list[0] + _list[1]) % 2 == 0:
-                _list[2] = (_list[0] + _list[1]) // 2
-            else:
-                _list[2] = (_list[0] + _list[1]) // 2 + 1
-        else:
-            if (_list[1] + _list[2]) % 2 == 0:
-                _list[0] = (_list[1] + _list[2]) // 2
-            else:
-                _list[0] = (_list[1] + _list[2]) // 2 + 1
-
-    _list.sort()
-
-print(cost)
+if diff_a_b == 1 and diff_b_c == 1:
+    print(0)
+elif diff_a_b == 2 or diff_b_c == 2:
+    print(1)
+else:
+    print(2)
