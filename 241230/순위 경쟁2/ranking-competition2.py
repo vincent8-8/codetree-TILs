6b9f424ec66b377199c809766129331357fd1bb7
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-12-30 19:36:36 +0900

    Title: 순위 경쟁2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241230/순위 경쟁2/ranking-competition2.py
+++ 241230/순위 경쟁2/ranking-competition2.py
@@ -3,12 +3,12 @@ _list = [list(input().split()) for _ in range(n)]
 
 a_score = 0
 b_score = 0
-top_rating = ''
+top_rating = 'A, B'
 answer = 0
 
 for person, score in _list:
     score = int(score)
-    
+
     if person == 'A':
         a_score += score
     else:
@@ -19,7 +19,7 @@ for person, score in _list:
             pass
         else:
             answer += 1
-            top_rating = "A , B"
+            top_rating = "A, B"
     elif a_score < b_score:
         if top_rating == 'B':
             pass
