d104372e05e73d0d9292ab256eec8ccc90b3949f
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-12-31 14:32:38 +0900

    Title: L, R 그리고 B, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241231/L, R 그리고 B/l-r-and-b.py
+++ 241231/L, R 그리고 B/l-r-and-b.py
@@ -11,9 +11,9 @@ for row in range(10):
 ver_dist = (abs(l_b_cordinate[0][0] - l_b_cordinate[1][0]) - 1) if abs(l_b_cordinate[0][0] - l_b_cordinate[1][0]) > 1 else 0
 hor_dist = (abs(l_b_cordinate[0][1] - l_b_cordinate[1][1]) - 1) if abs(l_b_cordinate[0][1] - l_b_cordinate[1][1]) > 1 else 0
 
-if ver_dist == 0 and hor_dist == 0:
-    print(0)
-elif ver_dist >= 1 and hor_dist >= 1:
-    print(ver_dist + hor_dist + 1)
+if l_b_cordinate[0][0] == l_b_cordinate[1][0]:
+    print(abs(l_b_cordinate[0][1] - l_b_cordinate[1][1]) - 1)
+elif l_b_cordinate[0][1] == l_b_cordinate[1][1]:
+    print(abs(l_b_cordinate[0][0] - l_b_cordinate[1][0]) - 1)
 else:
-    print(ver_dist + hor_dist)
+    print(ver_dist + hor_dist + 1)
