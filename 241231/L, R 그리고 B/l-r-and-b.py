4bddf7028f89e6a3fe2cfe45835b48894613be99
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2024-12-31 14:27:16 +0900

    Title: L, R 그리고 B, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241231/L, R 그리고 B/l-r-and-b.py
@@ -0,0 +1,19 @@
+_graph = [input() for _ in range(10)]
+l_b_cordinate = []
+
+for row in range(10):
+    for column in range(10):
+        if _graph[row][column] == 'B':
+            l_b_cordinate.append([row, column])
+        elif _graph[row][column] == 'L':
+            l_b_cordinate.append([row, column])
+
+ver_dist = (abs(l_b_cordinate[0][0] - l_b_cordinate[1][0]) - 1) if abs(l_b_cordinate[0][0] - l_b_cordinate[1][0]) > 1 else 0
+hor_dist = (abs(l_b_cordinate[0][1] - l_b_cordinate[1][1]) - 1) if abs(l_b_cordinate[0][1] - l_b_cordinate[1][1]) > 1 else 0
+
+if ver_dist == 0 and hor_dist == 0:
+    print(1)
+elif ver_dist >= 1 and hor_dist >= 1:
+    print(ver_dist + hor_dist + 1)
+else:
+    print(ver_dist + hor_dist)
