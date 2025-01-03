321f3349d1aa4cf3a11b7e9b285fbb56f9c75d5f
LEE SEONBIN <156335932+vincent8-8@users.noreply.github.com>
2025-01-03 15:45:54 +0900

    Title: 삽입 정렬 구현, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250103/삽입 정렬 구현/implement-insertion-sort.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Write your code here!
+
+for i in range(1, n):
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+    
+    arr[j + 1] = key
+
+for elem in arr:
+    print(elem , end=" ")
